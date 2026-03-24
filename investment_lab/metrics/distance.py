49c394ef42919042030cabc8cefdb10fa96ee097
caglanaz <cagla.amiklioglu@dauphine.eu>
2026-03-24 20:15:12 +0100

    Add files via upload
    comments

--- investment_lab/metrics/distance.py
+++ investment_lab/metrics/distance.py
@@ -3,8 +3,10 @@ import pandas as pd
 
 
 def mse(y_true: np.ndarray | pd.Series, y_pred: np.ndarray | pd.Series) -> float:
+    """Return the mean squared error between true and predicted values."""
     return float(np.mean((y_true - y_pred) ** 2))
 
 
 def sse(y_true: np.ndarray | pd.Series, y_pred: np.ndarray | pd.Series) -> float:
+    """Return the sum of squared errors between true and predicted values."""
     return np.sum((y_true - y_pred) ** 2)
